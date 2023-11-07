4790377e70b80877051593db64e43c8bfc9ee192
Sifat Ikram <sifatikram@gmail.com>
2023-11-07 13:07:41 +0600

    delete added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 4321;
 
 // Middleware
@@ -42,6 +42,13 @@ async function run() {
         const cursor = jobCollection.find();
         const result = await cursor.toArray();
         res.send(result);
+    })
+
+    app.delete('/allJobs/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await jobCollection.deleteOne(query);
+       res.send();
     })
 
 
