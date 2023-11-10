53356c17052fe293c26a993278ff397a721810c9
Sifat Ikram <sifatikram@gmail.com>
2023-11-10 17:58:28 +0600

    server side completed

--- index.js
+++ index.js
@@ -8,9 +8,29 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 4321;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173','https://job-hustle.web.app','https://job-hustle.firebaseapp.com'],
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
+
+const logger =(req, res, next)=>{
+  console.log('log: info', req.method, req.url);
+  next();
+}
+
+const verifyToken = (req, res, next)=>{
+  const token = req?.cookies?.token;
+  console.log('verify token', token);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+    if(err){
+      return res.status(401).send({message: 'Access denied'});
+    }
+    req.user = decoded;
+    next();
+  })
+}
 
 
 
@@ -29,10 +49,28 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const jobCollection = client.db('jobDB').collection('allJobs');
 
+    app.post('/jwt', async(req, res)=>{
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+      res
+      .cookie('token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production'? 'none' : 'strict'
+      })
+      .send({message: 'succeed', token});
+    })
+
+    app.post('/logOut', async(req, res)=>{
+      const user = req.body;
+      console.log('logging out', user);
+      res.clearCookie('token', {maxAge: 0}).send({success: true})
+    })
 
     app.post('/allJobs', async (req, res) => {
       const newJob = req.body;
@@ -47,17 +85,24 @@ async function run() {
       res.send(result);
     })
 
-    app.delete('/allJobs/:id', async (req, res) => {
+    app.delete('/myJob/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
       const result = await jobCollection.deleteOne(query);
       res.send(result);
     })
 
-    app.get('/allJobs/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
-      const result = await jobCollection.findOne(query);
+    app.get('/myJob', logger, verifyToken, async (req, res) => {
+      console.log(req.query.email);
+      console.log( 'token owner', req.user);
+      if(req.user.email !== req.query.email){
+        return res.status(403).send({message: 'Access forbidden'});
+      }
+      let query = {};
+      if(req.query?.email){
+        query = { email: req.query.email }
+        }
+      const result = await jobCollection.find(query).toArray();
       res.send(result);
     })
 
